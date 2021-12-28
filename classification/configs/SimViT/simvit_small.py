a13e9db2845de2f9482ebc91c9e201c8a904e840
ligang <ucasligang@163.com>
2021-12-28 23:46:33 +0800

    Update simvit_small.py

--- classification/configs/SimViT/simvit_small.py
+++ classification/configs/SimViT/simvit_small.py
@@ -1,8 +1,8 @@
 cfg = dict(
-    model='capt_small',
+    model='simvit_small',
     drop_path=0.1,
     clip_grad=None,
     #batch_size=128,
     batch_size=128,
-    output_dir='checkpoints/capt_small',
+    output_dir='checkpoints/simvit_small',
 )
