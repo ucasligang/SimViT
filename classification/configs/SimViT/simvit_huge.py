d1f5f88320c2456522331e8446e755870c077c0d
ligang <ucasligang@163.com>
2021-12-28 23:45:13 +0800

    Update simvit_huge.py

--- classification/configs/SimViT/simvit_huge.py
+++ classification/configs/SimViT/simvit_huge.py
@@ -1,7 +1,7 @@
 cfg = dict(
-    model='capt_huge',
+    model='simvit_huge',
     drop_path=0.3,
     clip_grad=1.0,
     batch_size=256,
-    output_dir='checkpoints/capt_huge',
+    output_dir='checkpoints/simvit_huge',
 )
