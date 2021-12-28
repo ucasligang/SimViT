e126628873287473e5b25d93d08d18ef66cbb0df
ligang <ucasligang@163.com>
2021-12-28 23:45:55 +0800

    Update simvit_medium.py

--- classification/configs/SimViT/simvit_medium.py
+++ classification/configs/SimViT/simvit_medium.py
@@ -1,7 +1,7 @@
 cfg = dict(
-    model='capt_medium',
+    model='simvit_medium',
     drop_path=0.3,
     clip_grad=1.0,
     batch_size=88,
-    output_dir='checkpoints/capt_medium_finetune',
+    output_dir='checkpoints/simvit_medium',
 )
