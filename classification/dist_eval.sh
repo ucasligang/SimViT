2cd04545fad5f1fe0f06acb8fe967fe24498040b
ligang <ucasligang@163.com>
2021-12-29 10:22:33 +0800

    Update dist_eval.sh

--- classification/dist_eval.sh
+++ classification/dist_eval.sh
@@ -3,10 +3,12 @@ export NCCL_LL_THRESHOLD=0
 
 CONFIG=$1
 GPUS=$2
+data_path=$3
 PORT=${PORT:-6666}
+resume=$4
 
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    --use_env main.py --config $CONFIG --resume /pub/data/ligang/projects/PVT/checkpoints/capt_small/checkpoint_max.pth --eval
+    --use_env main.py --config $CONFIG --data-path $data_path --resume $resume --eval
 
 
 # sh dist_eval.sh configs/pvt/pvt_small.py 1 --data-path /path/to/imagenet --resume /path/to/checkpoint_file --eval
