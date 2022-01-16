cc82c24086168101e96c241174f6cc2882069fda
ligang <ucasligang@163.com>
2022-01-16 17:46:52 +0800

    Update simvit_medium.py

--- classification/configs/SimViT/simvit_medium.py
+++ classification/configs/SimViT/simvit_medium.py
@@ -1,7 +1,9 @@
 cfg = dict(
     model='simvit_medium',
-    drop_path=0.3,
-    clip_grad=1.0,
+    #drop_path=0.3,
+    drop_path=0.1,
+    #clip_grad=1.0,
+    clip_grad=None,
     batch_size=88,
     output_dir='checkpoints/simvit_medium',
 )
