7bea09136413948ef36deff56f7fee3ff60fa092
ligang <ucasligang@163.com>
2022-01-16 17:46:26 +0800

    Update simvit_large.py

--- classification/configs/SimViT/simvit_large.py
+++ classification/configs/SimViT/simvit_large.py
@@ -1,7 +1,9 @@
 cfg = dict(
     model='simvit_large',
-    drop_path=0.3,
-    clip_grad=1.0,
+    #drop_path=0.3,
+    drop_path=0.1,
+    #clip_grad=1.0,
+    clip_grad=None,
     batch_size=256,
     output_dir='checkpoints/simvit_large',
 )
